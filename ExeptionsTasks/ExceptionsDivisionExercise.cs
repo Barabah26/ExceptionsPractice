a4dbcaa97ce9cfb1a1cd40b11ef3fc06f7252094
Pavlo Barabakh <pavlo.barabakh@everymatrix.com>
2026-02-25 20:56:27 +0200

    Coding Exercise 28: Try-catch-finally. DivideNumbers

---
+++ ExeptionsTasks/ExceptionsDivisionExercise.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public static class ExceptionsDivisionExercise
+{
+    public static int DivideNumbers(int a, int b)
+    {
+        try
+        {
+            return a / b;
+        }
+        catch
+        {
+            Console.WriteLine("Division by zero.");
+        }
+        finally
+        {
+            Console.WriteLine("The DivideNumbers method ends.");
+        }
+        return 0;
+    }
+}
